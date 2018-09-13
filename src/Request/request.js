a374759446921f40bf0e87aade6bf1920f8d7a75
Christian Chavez <christiancchavez10@hotmail.com>
2018-09-13 16:02:58 -0500

    create all html file for project

--- src/Request/request.js
+++ src/Request/request.js
@@ -1,19 +1,24 @@
 import { bookStore } from '../Utils/request';
 
 const requestGetBook = () => {
-    return bookStore.get(`books`);
+    return bookStore.get('books');
 };
 
 const requestGetCharacters = () => {
-    return bookStore.get(`characters`);
+    return bookStore.get('characters');
 };
 
 const requestGetHouses = () => {
-    return bookStore.get(`houses`);
+    return bookStore.get('houses');
 };
+
+const requestGetHouse = (houseId) => {
+    return bookStore.get(`houses/${houseId}`);
+}
 
 export {
     requestGetBook,
     requestGetCharacters,
     requestGetHouses, 
+    requestGetHouse,
 };
